717ffabfa31df07424a08e4815b151b176f42d5d
Aaron Krause <aaroncrows@Aarons-MacBook-Pro.local>
2017-01-23 13:40:31 -0800

    comments stripped

--- webpack.config.js
+++ webpack.config.js
@@ -7,52 +7,22 @@ const paths = {
   build: join(__dirname, '/build/')
 }
 
-// export a configuration object
-
 module.exports = {
-  // entry is the path to the main source file
-  // (usually the index)
   entry: paths.entry,
-  // output takes an object which describes the output bundle
   output: {
-    // the path to the directory where the build is going to go
     path: paths.build,
-    // the name of the ouput bundle
     filename: 'bundle.js'
   },
-  // module is where your loader configuration goes.
-  // webpack handles things that aren't necessarily javascript
-  // so loaders help webpack handle that
   module: {
-    // rules takes an array of objects.
     rules: [
       {
-        // test tells webpack which files to run through this loader
-        // here we're giving it a regex that will run this loader configuration
-        // on all files that have an extenstion of .js or .jsx
         test: /\.jsx?$/,
-        // exclude allows you to set a things to leave out of this config
         exclude: './node_modules/',
-        // here we designate which loader or loaders to use. The loader property
-        // takes a string where the loaders property takes an array. If you want
-        // to use the query property you have to use loader
         loader: 'babel-loader',
-        // query allows us to pass configuration options to the loader itself
-        // it can also be attached to the string set to loader as a query string.
-        // Babel itself is just the transpiler so you need to tell it which
-        // transpilations to run. They also need to be installed seperately.
-        // these two are in the package.json (babel-preset-es2015 and
-        // babel-preset-react.) One is for jsx and the other is for es2015
-        // syntax like import.
         options: {
           presets: ['react', 'es2015']
         }
       },
-      // webpack even allows us to bring in files that aren't javascript into our
-      // bundle. In this case we're bringing in our stylesheets. In order to create
-      // a loader that understands adding CSS to the bundle we use another plugin.
-      // ExtractTextPlugin has a method extract. That method takes an object that has
-      // a property "loader". That loader can be configured like any other loader.
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
@@ -61,18 +31,8 @@ module.exports = {
       }
     ]
   },
-  // Webpack even grants the capability to generate files that didn't previously exist
-  // we use plugins for that. The plugins property takes an array of plugins. Plugins
-  // are third party so the way they're configured varies but often times it involves
-  // instantiating the plugin and passing in an object with your configuration options.
   plugins: [
-    // HtmlWebpackPlugin generates an index.html in the directory pointed to by output.path.
-    // By default it generates it with a script tag at the bottom that points to the bundle
-    // that webpack created.
     new HtmlWebpackPlugin(),
-    // ExtractTextPlugin takes an argument of the filename for the style file that it creates
-    // in the directory pointed to by output.path. It even tells HtmlWebpackPlugin to add a
-    // link with this src to the head of your newly generated index.html!
     new ExtractTextPlugin('style.css')
   ]
 }
